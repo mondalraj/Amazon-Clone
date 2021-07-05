dcea0d8f1161829271324458c1647b1e2874df59
Rajib Mondal <mondal021002rajib@gmail.com>
2021-07-05 10:59:22 +0530

    amazon header and banner completed

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDl8POWxovbSwWPGPKJ-XhenPN_F-rPKQs",
+    authDomain: "e-shop-pro.firebaseapp.com",
+    projectId: "e-shop-pro",
+    storageBucket: "e-shop-pro.appspot.com",
+    messagingSenderId: "207946889172",
+    appId: "1:207946889172:web:033805335a1d1e75750841",
+    measurementId: "G-8BZXSW8VPL"
+  };
